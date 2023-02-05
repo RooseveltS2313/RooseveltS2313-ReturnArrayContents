633e0900394e2a7c1e08b1459a9eb6c35c9c9da5
Roosevelt Seymour <RSeymourJR04@outlook.com>
2023-02-05 21:02:56 +0000

    Completed Return Array R Seymour Jr

--- src/main/java/ReturnArrayContents.java
+++ src/main/java/ReturnArrayContents.java
@@ -28,6 +28,12 @@ public class ReturnArrayContents {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        String content = "";
+        for( int i = 0; i < nums.length; i++){
+            // It automatically parses with this?
+            content += (nums[i]) + " " ;
+
+        }
+        return content;
     }
 }
